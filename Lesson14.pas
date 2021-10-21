6385e360accf1432b4b7c3a2d5905eafa9057407
Konoplyanova Anna <konoplianova@sfedu.ru>
2021-10-21 09:29:02 +0300

    Add func LaterInYear

--- Lesson14.pas
+++ Lesson14.pas
@@ -2,7 +2,20 @@
 
 function IsLeapYear(year: integer) := (year mod 4 = 0) and (year mod 100 <> 0);
 
-function SecondsInMinute() := 60;
+function LaterInYear(day1,day2,month1,month2: integer) : string;
+begin
+if month2 > month1 then
+  result := $'{day2}.{month2}'
+else if month1 > month2 then
+  result := $'{day1}.{month1}'
+else
+  begin
+  if day1 > day2 then
+    result := $'{day1}.{month1}'
+  else
+    result := $'{day2}.{month2}';
+  end;
+end;
 
 begin
 end.
